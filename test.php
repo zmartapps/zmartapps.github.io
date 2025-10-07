68507c1a12e0652818c808448c3335d673a06c5f
zmartapps <69975291+zmartapps@users.noreply.github.com>
2025-10-07 22:05:31 +0100

    Create test.php

---
+++ test.php
@@ -0,0 +1,19 @@
+<?php
+
+header ('Content-type: text/html; charset=utf-8');
+// or
+header ('Content-type: text/html; charset=iso8859-15');
+
+        header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+        header('Access-Control-Allow-Credentials: true');
+        header('Access-Control-Max-Age: 86400');    // cache for 1 day
+        header("Access-Control-Allow-Methods: GET, POST, OPTIONS");    
+    header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Request-With');
+
+echo "4444";
+
+?>
